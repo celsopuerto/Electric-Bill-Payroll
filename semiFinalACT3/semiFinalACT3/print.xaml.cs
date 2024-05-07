2babe808bc5d81b5ef9d79be470baa6bae5ce3b9
Celso M. Puerto Jr. <isoygwapo84@gmail.com>
2024-05-07 09:38:48 +0800

    Add project files.

---
+++ semiFinalACT3/semiFinalACT3/print.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semiFinalACT3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class print : ContentPage
+    {
+        public print()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            string status = $"{Application.Current.Properties["status"]}";
+            int meter = int.Parse($"{Application.Current.Properties["data"]}");
+
+            var data = await App.db.SearchAsync(meter);
+            if (data != null)
+            {
+                if(status == "add")
+                {
+                    await DisplayAlert("Success", "Data Passed and Added Successfully", "OK");
+                    printData.ItemsSource = new List<table> { data };
+                }
+                else if(status == "update")
+                {
+                    await DisplayAlert("Success", "Data Passed and Updated Successfully", "OK");
+                    printData.ItemsSource = new List<table> { data };
+                }
+                else if(status == "delete")
+                {
+                    await DisplayAlert("Success", "Data Deleted Successfully", "OK");
+                    printData.ItemsSource = new List<table> { data };
+                    await App.db.DeleteAsync(data);
+                }
+            }
+            else
+            {
+                await DisplayAlert("Notice!", "Data Doesn't Exist!", "OK");
+            }
+
+
+        }
+
+        private void Back(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
